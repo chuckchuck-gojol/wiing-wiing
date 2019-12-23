268650f7e0b1aeac3ba8d409434441309abd3c46
mjung1798 <mor2222@naver.com>
2019-12-23 17:45:15 +0900

    record 확인

--- app/src/main/java/com/jyami/wiingwiing_black/ui/help/HelpFragment.java
+++ app/src/main/java/com/jyami/wiingwiing_black/ui/help/HelpFragment.java
@@ -1,9 +1,11 @@
 package com.jyami.wiingwiing_black.ui.help;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -13,10 +15,13 @@ import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 
 import com.jyami.wiingwiing_black.R;
+import com.jyami.wiingwiing_black.RecordActivity;
 
 public class HelpFragment extends Fragment {
 
     private HelpViewModel helpViewModel;
+    Intent intent;
+    public Button button = null;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -25,6 +30,16 @@ public class HelpFragment extends Fragment {
 
         View root = inflater.inflate(R.layout.fragment_help, container, false);
 
+        intent = new Intent(getActivity(), RecordActivity.class);
+        button = (Button) root.findViewById(R.id.theButton);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                startActivity(intent);
+            }
+        });
+
         return root;
     }
+
+
 }
