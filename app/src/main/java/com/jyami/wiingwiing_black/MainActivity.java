d05f564b0403aff1aa89e06e4315e2fe62280422
mjung1798 <mor2222@naver.com>
2020-06-16 21:01:50 +0900

    Fix: fix some invalid import

--- app/src/main/java/com/jyami/wiingwiing_black/MainActivity.java
+++ app/src/main/java/com/jyami/wiingwiing_black/MainActivity.java
@@ -14,12 +14,6 @@ import androidx.navigation.ui.NavigationUI;
 import com.google.android.material.navigation.NavigationView;
 import com.jyami.wiingwiing_black.ui.Utils;
 
-import org.python.util.PythonInterpreter;
-
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-
 public class MainActivity extends AppCompatActivity {
 
     private AppBarConfiguration mAppBarConfiguration;
@@ -47,13 +41,6 @@ public class MainActivity extends AppCompatActivity {
         NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
         NavigationUI.setupWithNavController(navigationView, navController);
 
-//        System.setProperty("python.cachedir.skip", "true");
-        PythonInterpreter.initialize(System.getProperties(), System.getProperties(), new String[0]);
-        PythonInterpreter interpreter = new PythonInterpreter();
-        interpreter.exec("print('hello')");
-
-
-//        tfLite.runForMultipleInputsOutputs();
     }
 
     @Override
